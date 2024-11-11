83786c8131f246bbbcb5f65528d84c70ad707620
ParthSauguny <parthsauguny@gmail.com>
2024-11-12 00:35:47 +0530

    started

--- Client-Side/src/App.jsx
+++ Client-Side/src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react'
+import Sidebar from './Components/Side-bar'
 import './App.css'
 
 function App() {
-
+    return (
+        <>
+            <Sidebar/>
+            
+        </>
+    )
 }
 
 export default App
---
+++ Client-Side/src/Components/Side-bar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='bg-violet-700 flex flex-col h-screen w-1/6'>
+        <button className='mx-auto mt-4 border-4 border-slate-400 bg-black text-white rounded-2xl p-2 font-semibold font-serif'>+ New Chat</button>
+        <ul>
+
+        </ul>
+    </div>
+  )
+}
+
+export default Sidebar
