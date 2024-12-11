a9aa96b97f86dedcefcfcce36704808255decf20
ParthSauguny <parthsauguny@gmail.com>
2024-12-11 23:12:28 +0530

    added auth middleware

--- Server-Side/middlewares/auth.js
+++ Server-Side/middlewares/auth.js
@@ -1,3 +1,28 @@
-const auth = (req , next , res) => {
-    const token = req.Headers.cookie.AccessToken
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const User = require('../models/user');
+
+const auth = (req , res , next) => {
+    try {
+        const token = req.cookie.AccessToken;
+
+        if(!token){
+            return res.status(400).json({message: "unauthorised access"});
+        }
+
+        const decoded = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET);
+
+        const user = User.findById(decoded?._id).select("-password -refreshtoken");
+
+        if(!user){
+            return res.status(401).json({message: "user not found"});
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({message: "request not valid"});
+    }
 }
+module.exports = auth;
